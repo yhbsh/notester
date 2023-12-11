b61a0eda5652244be5d053b06189c975b3d324b5
HoussemBousmaha <ky_bousmaha@esi.dz>
2023-12-11 05:59:44 +0300

    A simple ListView with 20 scrolling items

--- app/src/main/java/com/example/notester/MainActivity.java
+++ app/src/main/java/com/example/notester/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.notester;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listview = findViewById(R.id.listView);
+
+        String[] items = new String[20];
+
+        for (int i = 0; i < 20; i++) {
+            items[i] = "Hello World";
+        }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.list_item, items);
+        listview.setAdapter(arrayAdapter);
     }
 }
