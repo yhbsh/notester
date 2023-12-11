879f5ae7d83095f846892a47187a5ed72e8c857c
HoussemBousmaha <ky_bousmaha@esi.dz>
2023-12-11 06:04:34 +0300

    Add simple item click

--- app/src/main/java/com/example/notester/MainActivity.java
+++ app/src/main/java/com/example/notester/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.notester;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,15 +14,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listview = findViewById(R.id.listView);
+        ListView listView = findViewById(R.id.listView);
 
-        String[] items = new String[20];
+        String[] items = new String[100];
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 100; i++) {
             items[i] = "Hello World";
         }
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.list_item, items);
-        listview.setAdapter(arrayAdapter);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            // Code to execute on item click
+
+            // Example: display a Toast with the clicked item's text
+            String selectedItem = (String) parent.getItemAtPosition(position);
+            Toast.makeText(MainActivity.this, "Clicked: " + selectedItem + " " + position, Toast.LENGTH_SHORT).show();
+        });
+
     }
 }
